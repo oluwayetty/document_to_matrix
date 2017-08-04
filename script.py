5b1dbd9180c3b1c06ae61dd2388f8ea137849605
Yetunde Sanni <yetunde.sanni@andela.com>
2017-08-04 12:02:05 +0100

    refactored code to look sexy

--- script.py
+++ script.py
@@ -2,23 +2,21 @@ import re, math
 from collections import Counter
 import numpy as np
 
-
-
 text1 = 'How can I be a geologist?'
 text2 = 'What should I do to be a geologist?'
 
-class Similarity
-    def compute_cosine_similarity(vec1, vec2):
+class Similarity():
+    def compute_cosine_similarity(self, string1, string2):
          # intersects the words that are common
          # in the set of the two words
-         intersection = set(vec1.keys()) & set(vec2.keys())
+         intersection = set(string1.keys()) & set(string2.keys())
          # dot matrix of vec1 and vec2
-         numerator = sum([vec1[x] * vec2[x] for x in intersection])
+         numerator = sum([string1[x] * string2[x] for x in intersection])
 
          # sum of the squares of each vector
          # sum1 is the sum of text1 and same for sum2 for text2
-         sum1 = sum([vec1[x]**2 for x in vec1.keys()])
-         sum2 = sum([vec2[x]**2 for x in vec2.keys()])
+         sum1 = sum([string1[x]**2 for x in string1.keys()])
+         sum2 = sum([string2[x]**2 for x in string2.keys()])
 
          # product of the square root of both sum(s)
          denominator = math.sqrt(sum1) * math.sqrt(sum2)
@@ -27,25 +25,32 @@ class Similarity
          else:
             return round(numerator/float(denominator),4)
 
-    def text_to_vector(text):
+    def text_to_vector(self,text):
         WORD = re.compile(r'\w+')
         words = WORD.findall(text)
         return Counter(words)
 
-    vector1 = text_to_vector(text1)
-    vector2 = text_to_vector(text2)
-
     # Jaccard Similarity
-    def tokenize(string):
+    def tokenize(self,string):
         return string.lower().split(" ")
 
-    def jaccard_similarity(string1, string2):
+    def jaccard_similarity(self, string1, string2):
         intersection = set(string1).intersection(set(string2))
         union = set(string1).union(set(string2))
         return len(intersection)/float(len(union))
 
-cosine = Similarity.compute_cosine_similarity(vector1, vector2)
+similarity = Similarity()
+
+# vector space
+vector1 = similarity.text_to_vector(text1)
+vector2 = similarity.text_to_vector(text2)
+
+# split words into tokens
+token1 = similarity.tokenize(text1)
+token2 = similarity.tokenize(text2)
+
+cosine = similarity.compute_cosine_similarity(vector1, vector2)
 print 'Cosine Similarity:', cosine
 
-jaccard = Similarity.jaccard_similarity(tokenize(text1),tokenize(text2))
+jaccard = similarity.jaccard_similarity(token1,token2)
 print 'Jaccard Similarity:', jaccard
